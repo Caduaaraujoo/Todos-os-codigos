fdbcff5bfc3be7b057d1eccd9d5a5e33d0be1f25
Cadu Araujo <caduaaraujoo@gmail.com>
2021-11-12 15:28:17 -0300

    Todos Testes

--- cubos.js
+++ cubos.js
@@ -1,6 +1,50 @@
 
-function solucao(presenca, entregouTodosOsTrabalhos, notaSoftSkills, notaProgramacao)
-{
+function solucao(presenca, entregouTodosOsTrabalhos, notaSoftSkills, notaProgramacao){
+if (presenca <75 && entregouTodosOsTrabalhos === false){
+    console.log("REPROVADA")
+} else if (presenca >= 75 && entregouTodosOsTrabalhos === true){
+    console.log("APROVADA")
+} else if (notaSoftSkills >= 90 && notaProgramacao <=89){
+    console.log("APROVADA - MERITO EM SOFT SKILLS")
+} else if (notaSoftSkills <= 89 && notaProgramacao >= 90){
+    console.log("APROVADA - MERITO EM PROGRAMACAO")
+} else if (notaSoftSkills >=90 && notaProgramacao >= 90){
+    console.log("APROVADA - NOTA 1000")
+}
+
+}
+
+
+function solucao(presenca, entregouTodosOsTrabalhos, notaSoftSkills, notaProgramacao){
+    if (presenca <75 && entregouTodosOsTrabalhos === false){
+        console.log("REPROVADA")
+    } else if (presenca >= 75 && entregouTodosOsTrabalhos === true){
+        console.log("APROVADA")
+    } else if (presenca >=75 && entregouTodosOsTrabalhos === true){
+        if (notaSoftSkills >=90){
+            console.log("APROVADA - MERITO EM SOFT SKILLS")
+        } else {
+            console.log ("APROVADA")
+        }
+    } else if (presenca >= 75 && entregouTodosOsTrabalhos === true){
+        if (notaProgramacao >=90){
+            console.log("APROVADA - MERITO EM PROGRAMACAO")
+        } else {
+            console.log("APROVADA")
+        }
+    } else if (presenca >= 75 && entregouTodosOsTrabalhos === true){
+        if (notaSoftSkills >=90 && notaProgramacao >=90){
+            console.log("APROVADA - NOTA 1000")
+        } else {
+            console.log("APROVADA")
+        }
+    }
+    
+    }
+    
+    
+
+
     if (presenca >= 75 && entregouTodosOsTrabalhos ===true && notaSoftSkills >= 90 &&notaProgramacao >= 90){
         console.log("APROVADA - NOTA 1000")}
         else if (presenca >= 75 && entregouTodosOsTrabalhos === true && notaSoftSkills >= 90) {
@@ -13,4 +57,4 @@ function solucao(presenca, entregouTodosOsTrabalhos, notaSoftSkills, notaProgram
             console.log("REPROVADA")
         }
         
-    }
+        
